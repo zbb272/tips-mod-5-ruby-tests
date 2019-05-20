268ffe732f2decefa2baeca3f1b1b97c2b6f8dbd
github username <github email address>
2019-05-20 12:13:21 -0400

    Done.

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -10,21 +10,20 @@ class RecurringMoment
 
   def match(date)
     current = @start
-
     while current < date
-      if current == date
-        return true
-      end
-
       if @period == 'monthly'
-        current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
         current = current.advance(months: @interval)
+      elsif @period == 'weekly'
+        current = current.advance(weeks: @interval)
       elsif @period == 'daily'
         current = current.advance(days: @interval)
       end
     end
+    if current == date
+      return true
+    end
 
     return false
   end
+
 end
